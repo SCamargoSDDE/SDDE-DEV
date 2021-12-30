6be587caafbb98ac5862c0745c638c89d98af868
SCamargoSDDE <scamargo@desarrolloeconomico.gov.co>
2021-12-29 22:50:11 -0500

    Campos Fomulario 3
    Se modelo el html del formulario

--- src/app/components/forms-emprendimiento/form-three/form-three.component.ts
+++ src/app/components/forms-emprendimiento/form-three/form-three.component.ts
@@ -19,29 +19,13 @@ export class FormThreeComponent implements OnInit {
     private snackBar: MatSnackBar) {
 
     this.myForm = this.fb.group({
-      emplendimientoConstituido: ['', [Validators.required]],
-      numeroDocumento: ['', [Validators.required]],
-      primerApellido: ['', [Validators.required]],
-      segundoApellido: ['', []],
-      primerNombre: ['', [Validators.required]],
-      segundoNombre: ['', []],
-      c1: ['', [Validators.required]],
-      c2: ['', [Validators.required]],
-      c3: ['', []],
-      c4: ['', []],
-      c5: ['', []],
-      c6: ['', []],
-      c7: ['', [Validators.required]],
-      c8: ['', []],
-      c9: ['', []],
-      c10: ['', []],
-      correoElectronico: ['', [Validators.required, Validators.email]],
-      confirmacionCorreo: ['', [Validators.required, Validators.email]],
-      fechaNacimiento: ['', [Validators.required]],
-      numeroCelular: ['', [Validators.required]],
-      numeroFijo: ['', []],
-      contrasena: ['', [Validators.required]],
-      confirmarContrasena: ['', [Validators.required]]
+      sectorProductivo: ['', [Validators.required]],
+      intereses: ['', [Validators.required]],
+      numeroColaboradores: ['', [Validators.required]],
+      descripcionEmprendimiento: ['', []],
+      publicoEmprendimiento: ['', [Validators.required]],
+      dpropuestaEmprendimiento: ['', []],
+      problemaEmprendimiento: ['', [Validators.required]]
     });
 
   }
